38fec9148b78f486a92b6fe520e12f1af6b026a7
Ying Wun Yvonne Ng <yvonne.ng@cern.ch>
2023-02-01 09:09:08 +0100

    cleaning

--- draw_multiple_from_hists.py
+++ draw_multiple_from_hists.py
@@ -13,11 +13,7 @@ from config.signal_file_list import signal_list as signal_filelist
 from config.bkg_file_list import dec_bkg_file_list as bkg_filelist 
 
 #---1. Set all the variable names you are interested in running in the histogram
-#--Branch name can be tree branch or calculation from tree branch E.G.  pT[0]**2+pT[1]**2
-branch_names=binning_dict.keys()
-print(len(branch_names))
-print(branch_names)
-
+branchname="pt"
 
 #--2. Add in the cut/histogram name in root file
 compare_dict={"Mass0.4-Prompt-Cut1": "hist1", # histogram name
@@ -46,6 +42,6 @@ for name, hist_name in compare_dict:
 	    hist_list.append(f1.Get(hist_name))
 
 try:
-	draw_summary(hist_list, compare_dict.keys(), branchname , saveDir=saveDir, drawLog=drawLog)
+    draw_summary(hist_list, compare_dict.keys(), branchname , saveDir=saveDir, drawLog=drawLog)
 except Exception as e:
-	print(e)
+    print(e)
